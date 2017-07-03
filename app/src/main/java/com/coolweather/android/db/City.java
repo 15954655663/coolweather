4cc43caef63ec8f639972de6ece9776017c057f0
lining <123820034@qq.com>
2017-07-03 10:40:58 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,23 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 李宁 on 2017/7/3.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+    public int getId(){return  id;}
+    public void setId(int id){this.id=id;}
+    public String getCityname(){return cityName;}
+    public void setCityName(String cityName){this.cityName=cityName;}
+    public int getCityCode(){return cityCode;}
+    public void setCityCode(int cityCode){this.cityCode=cityCode;}
+    public int getProvinceId(){return provinceId;}
+    public void setProvinceId(int provinceId){this.provinceId=provinceId;}
+
+}
---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,24 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+/**
+ * Created by 李宁 on 2017/7/2.
+ */
+
+public class Province extends DataSupport{
+private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){return provinceName;}
+    public void setProvinceName(String provinceName){this.provinceName=provinceName;}
+    public int getProvinceCode(){return provinceCode;}
+    public void setProvinceCode(int provinceCode){this.provinceCode=provinceCode;}
+
+}
